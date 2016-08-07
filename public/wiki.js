cd76a61be018cd3f8e692e09bc23136980cc17ea
Michael Sanford <mikethecodegeek@gmail.com>
2016-08-07 13:46:47 -0700

    css

--- public/wiki.js
+++ public/wiki.js
@@ -51,7 +51,23 @@ function init() {
         
     });
     
-    
+    $("#searchbox").autocomplete({
+    source: function(request, response) {
+        $.ajax({
+            url: "http://en.wikipedia.org/w/api.php",
+            dataType: "jsonp",
+            data: {
+                'action': "opensearch",
+                'format': "json",
+                'search': request.term
+            },
+            success: function(data) {
+                response(data[1]);
+            }
+        });
+    }
+});
+
     $('#clear').on("click", function() {
         $('#data').html('');
         $('#searchbox').val('');
